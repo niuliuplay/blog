148a60cd933f3f5c0a7a4d1082195b6926f720ae
liuhuihai <liuhuihail@163.com>
2018-06-16 08:52:43 +0800

    commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/* 
+* @Author: 刘辉海
+* @Date:   2018-06-16 08:46:40
+* @Last Modified by:   anchen
+* @Last Modified time: 2018-06-16 08:48:04
+*/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello github");
+    }
+}
